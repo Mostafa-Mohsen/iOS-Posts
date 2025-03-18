2f54081a6494bc16f38f817847da38f7ce783a5e
Mostafa-Mohsen <eng.mostafamohsen2016@gmail.com>
2025-03-18 23:14:14 +0200

    add search posts use case

--- Posts/Domain/UseCases/SearchPostsUseCase.swift
+++ Posts/Domain/UseCases/SearchPostsUseCase.swift
@@ -12,7 +12,22 @@ protocol SearchPostsUseCase: FetchUserUseCase {
     func execute(requestValue: SearchPostsUseCaseRequestValue) -> AnyPublisher<PostsPage?, NetworkError>
 }
 
+final class DefaultSearchPostsUseCase {
+    private let searchPostsRepository: SearchPostsRepository
+    var userRepository: FetchUserRepository { return searchPostsRepository }
+    
+    init(searchPostsRepository: SearchPostsRepository) {
+        self.searchPostsRepository = searchPostsRepository
+    }
+}
 
+extension DefaultSearchPostsUseCase: SearchPostsUseCase {
+    func execute(requestValue: SearchPostsUseCaseRequestValue) -> AnyPublisher<PostsPage?, NetworkError> {
+        return searchPostsRepository.getPosts(query: requestValue.query,
+                                              limit: requestValue.limit,
+                                              skip: requestValue.skip)
+    }
+}
 
 struct SearchPostsUseCaseRequestValue {
     let limit: Int
