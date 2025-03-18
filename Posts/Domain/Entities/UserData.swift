b67e305fe544293cc8c621f76909fcc8fe82af2e
Mostafa-Mohsen <eng.mostafamohsen2016@gmail.com>
2025-03-18 22:01:13 +0200

    add user data Entity

---
+++ Posts/Domain/Entities/UserData.swift
@@ -0,0 +1,14 @@
+//
+//  UserData.swift
+//  Posts
+//
+//  Created by M-M-M on 16/03/2025.
+//
+
+import Foundation
+
+struct UserData {
+    let id: Int
+    let firstName: String?
+    let lastName: String?
+}
