67ef378a58f558eebee6fe02549a329161e87f1b
Mostafa-Mohsen <eng.mostafamohsen2016@gmail.com>
2025-03-18 22:29:02 +0200

    add fetch user repository logic - fetch from local / remote + cache user data

--- Posts/Domain/Interfaces/FetchUserRepository.swift
+++ Posts/Domain/Interfaces/FetchUserRepository.swift
@@ -14,4 +14,59 @@ protocol FetchUserRepository {
     func getUsersBy(ids: [Int]) -> AnyPublisher<[UserData?], Never>
 }
 
-
+extension FetchUserRepository {
+    func getUsersBy(ids: [Int]) -> AnyPublisher<[UserData?], Never> {
+        return getCachedUsers(ids: ids)
+            .flatMap { cachedUsers in
+                let unwrappedCachedUsers = cachedUsers.compactMap({ $0 })
+                let cachedIds = unwrappedCachedUsers.map{ $0.id }
+                let remainingIds = ids.filter({ !cachedIds.contains($0) })
+                return getRemoteUsersBy(ids: remainingIds)
+                    .map { remoteUsers in
+                        return unwrappedCachedUsers + remoteUsers
+                    }
+            }
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+    
+    private func getRemoteUsersBy(ids: [Int]) -> AnyPublisher<[UserData?], Never> {
+        let publishers = ids.map { getRemoteUser(id: $0) }
+        return Publishers.MergeMany(publishers)
+                .collect()
+                .eraseToAnyPublisher()
+    }
+    
+    private func getRemoteUser(id: Int) -> AnyPublisher<UserData?, Never> {
+        let requestDTO = FetchUserDataRequestDTO(id: id)
+        let endpoint = APIEndpoints.getUserData(with: requestDTO)
+        
+        return networkService.request(endPoint: endpoint, type: FetchUserDataResponseDTO.self)
+            .map{ (postsResponse) -> UserData? in
+                guard let unwrappedPosts = postsResponse else { return nil }
+                cache.save(response: unwrappedPosts, for: requestDTO)
+                return unwrappedPosts.toDomain()
+            }
+            .replaceError(with: nil)
+            .eraseToAnyPublisher()
+    }
+    
+    private func getCachedUsers(ids: [Int]) -> AnyPublisher<[UserData?], Never> {
+        let publishers = ids.map { getCachedUser(id: $0) }
+        return Publishers.MergeMany(publishers)
+                .collect()
+                .eraseToAnyPublisher()
+    }
+    
+    private func getCachedUser(id: Int) -> AnyPublisher<UserData?, Never> {
+        let requestDTO = FetchUserDataRequestDTO(id: id)
+        
+        return cache.getResponse(for: requestDTO)
+            .map{ (postsResponse) -> UserData? in
+                guard let unwrappedPosts = postsResponse else { return nil }
+                return unwrappedPosts.toDomain()
+            }
+            .replaceError(with: nil)
+            .eraseToAnyPublisher()
+    }
+}
