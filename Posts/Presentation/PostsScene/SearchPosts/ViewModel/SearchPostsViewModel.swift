d0daf202391def99f4dae6c69a50deaf89e01871
Mostafa-Mohsen <eng.mostafamohsen2016@gmail.com>
2025-03-18 23:21:11 +0200

    add search posts view model

--- Posts/Presentation/PostsScene/SearchPosts/ViewModel/SearchPostsViewModel.swift
+++ Posts/Presentation/PostsScene/SearchPosts/ViewModel/SearchPostsViewModel.swift
@@ -38,3 +38,58 @@ protocol SearchPostsViewModelOutput {
     var errorDismissText: String { get }
 }
 
+typealias SearchPostsViewModel = SearchPostsViewModelInput & SearchPostsViewModelOutput
+
+final class DefaultSearchPostsViewModel: SearchPostsViewModelOutput {
+
+    private let searchPostsUseCase: SearchPostsUseCase
+    private let actions: SearchPostsViewModelActions?
+
+    private var query: String = ""
+    private var skip: Int = 0
+    private var totalCount: Int = 0
+    private var limit: Int = 4
+    private var hasMorePages: Bool { totalCount > skip }
+    private var posts: [Post] = []
+    private var cancellable: [AnyCancellable] = []
+    
+    
+    // MARK: - OUTPUT
+    let items = CurrentValueSubject<[PostsListItemViewModel], Never>([])
+    let loading = CurrentValueSubject<SearchPostsViewModelLoading, Never>(.none)
+    var showListLoader = PassthroughSubject<Bool, Never>()
+    let error = PassthroughSubject<String, Never>()
+    var isEmptyResults = PassthroughSubject<Bool, Never>()
+    let screenTitle = "Search"
+    let emptyResultsMessage = "No Result Found"
+    let errorTitle = "Error"
+    var errorDismissText = "Ok"
+
+    // MARK: - Init
+    init(searchPostsUseCase: SearchPostsUseCase,
+        actions: SearchPostsViewModelActions? = nil) {
+        self.searchPostsUseCase = searchPostsUseCase
+        self.actions = actions
+    }
+
+    
+}
+
+// MARK: - INPUT. View event methods
+extension DefaultSearchPostsViewModel: SearchPostsViewModelInput {
+    func didSearch(query: String) {
+        
+    }
+    
+    func loadNextPage() {
+        
+    }
+    
+    func didCancelSearch() {
+        
+    }
+    
+    func didClickOn(image: String) {
+        
+    }
+}
