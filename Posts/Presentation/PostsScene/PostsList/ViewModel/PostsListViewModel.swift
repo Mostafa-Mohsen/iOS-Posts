28b061b0df9832e48cceec3aa0e89519222e30d5
Mostafa-Mohsen <eng.mostafamohsen2016@gmail.com>
2025-03-18 21:34:36 +0200

    add posts view model output

--- Posts/Presentation/PostsScene/PostsList/ViewModel/PostsListViewModel.swift
+++ Posts/Presentation/PostsScene/PostsList/ViewModel/PostsListViewModel.swift
@@ -9,7 +9,6 @@ import Foundation
 import Combine
 
 
-
 enum PostsListViewModelLoading {
     case fullScreen
     case nextPage
@@ -21,6 +20,17 @@ protocol PostsListViewModelInput {
     func loadNextPage()
     func didClickSearch()
     func didClickOn(image: String)
+}
+
+protocol PostsListViewModelOutput {
+    var items: CurrentValueSubject<[PostsListItemViewModel], Never> { get }
+    var loading: CurrentValueSubject<PostsListViewModelLoading, Never> { get }
+    var showListLoader: PassthroughSubject<Bool, Never> { get }
+    var error: PassthroughSubject<String, Never> { get }
+    var screenTitle: String { get }
+    var errorTitle: String { get }
+    var errorDismissText: String { get }
+    var searchIcon: String { get }
 }
 
 
