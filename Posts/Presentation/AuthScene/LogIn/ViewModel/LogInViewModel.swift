526c4180f4db6e805843dec304b2d22f82e8def7
Mostafa-Mohsen <eng.mostafamohsen2016@gmail.com>
2025-03-18 19:57:37 +0200

    add log in view model output protocols

--- Posts/Presentation/AuthScene/LogIn/ViewModel/LogInViewModel.swift
+++ Posts/Presentation/AuthScene/LogIn/ViewModel/LogInViewModel.swift
@@ -8,10 +8,23 @@
 import Foundation
 import Combine
 
-
-
 protocol LogInViewModelInput {
     func didClickLogInWith(name: String, password: String)
 }
 
-
+protocol LogInViewModelOutput {
+    var errorTitle: String { get }
+    var welcomeImage: String { get }
+    var welcomeText: String { get }
+    var nameText: String { get }
+    var nameFieldPlaceholder: String { get }
+    var passwordText: String { get }
+    var passwordFieldPlaceholder: String { get }
+    var showPasswordImage: String { get }
+    var hidePasswordImage: String { get }
+    var signInButtonText: String { get }
+    var screenTitle: String { get }
+    var error: PassthroughSubject<String, Never> { get }
+    var errorDismissText: String { get }
+    var isLoading: CurrentValueSubject<Bool, Never> { get }
+}
