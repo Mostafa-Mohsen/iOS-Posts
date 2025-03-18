ec5fd44c5e49b2b9242599dc7bcab5a9fb26419d
Mostafa-Mohsen <eng.mostafamohsen2016@gmail.com>
2025-03-18 19:53:53 +0200

    add log in use case request

---
+++ Posts/Domain/UseCases/LogInUseCase.swift
@@ -0,0 +1,15 @@
+//
+//  LogInUseCase.swift
+//  Posts
+//
+//  Created by M-M-M on 15/03/2025.
+//
+
+import Foundation
+import Combine
+
+
+struct LogInsUseCaseRequestValue {
+    let name: String
+    let password: String
+}
