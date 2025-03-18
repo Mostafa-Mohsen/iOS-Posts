a33ddc5682b5aa85534a80497ae2a0312e5c11e5
Mostafa-Mohsen <eng.mostafamohsen2016@gmail.com>
2025-03-18 23:32:25 +0200

    add image preview view model input

---
+++ Posts/Presentation/PostsScene/ImagePreview/ViewModel/ImagePreviewViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ImagePreviewViewModel.swift
+//  Posts
+//
+//  Created by M-M-M on 16/03/2025.
+//
+
+import Foundation
+
+
+
+protocol ImagePreviewViewModelInput {
+    func didClickCloseImagePreview()
+}
+
+
