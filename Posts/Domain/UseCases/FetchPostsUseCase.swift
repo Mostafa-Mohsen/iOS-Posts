51c1e0d73d590df9aaf02ca5d4d990d1497ec72d
Mostafa-Mohsen <eng.mostafamohsen2016@gmail.com>
2025-03-18 21:18:56 +0200

    add posts use case request

---
+++ Posts/Domain/UseCases/FetchPostsUseCase.swift
@@ -0,0 +1,15 @@
+//
+//  FetchPostsUseCase.swift
+//  Posts
+//
+//  Created by M-M-M on 14/03/2025.
+//
+
+import Foundation
+import Combine
+
+
+struct PostsUseCaseRequestValue {
+    let limit: Int
+    let skip: Int
+}
