14bd151671acb5600a9174998e2fefc2442b0586
Mostafa-Mohsen <eng.mostafamohsen2016@gmail.com>
2025-03-18 19:35:51 +0200

    add log in Entity

---
+++ Posts/Domain/Entities/LogInToken.swift
@@ -0,0 +1,13 @@
+//
+//  LogInToken.swift
+//  Posts
+//
+//  Created by M-M-M on 15/03/2025.
+//
+
+import Foundation
+
+struct LogInToken {
+    let accessToken: String
+    let refreshToken: String
+}
