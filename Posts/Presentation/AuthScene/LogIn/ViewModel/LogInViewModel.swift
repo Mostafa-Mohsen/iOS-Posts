ad7f51fa57bfcd07dab5dc5b84bccd5a4d217aea
Mostafa-Mohsen <eng.mostafamohsen2016@gmail.com>
2025-03-18 19:59:51 +0200

    add log in view model

--- Posts/Presentation/AuthScene/LogIn/ViewModel/LogInViewModel.swift
+++ Posts/Presentation/AuthScene/LogIn/ViewModel/LogInViewModel.swift
@@ -33,3 +33,41 @@ protocol LogInViewModelOutput {
     var isLoading: CurrentValueSubject<Bool, Never> { get }
 }
 
+typealias LogInViewModel = LogInViewModelInput & LogInViewModelOutput
+
+final class DefaultLogInViewModel: LogInViewModelOutput {
+    
+    private let logInUseCase: LogInUseCase
+    private let actions: LogInViewModelActions?
+    private var cancellable: [AnyCancellable] = []
+    
+    // MARK: - OUTPUT
+    var isLoading = CurrentValueSubject<Bool, Never>(false)
+    let error = PassthroughSubject<String, Never>()
+    let errorTitle = "Error"
+    let errorDismissText = "Ok"
+    let screenTitle = "Log In"
+    let welcomeImage = "signin-image"
+    let welcomeText = "Welcome"
+    let nameText = "User Name"
+    let nameFieldPlaceholder = "Enter your user name"
+    let passwordText = "Password"
+    let passwordFieldPlaceholder = "Enter user password"
+    let showPasswordImage = "eye"
+    let hidePasswordImage = "eye.slash"
+    var signInButtonText = "Sign in"
+    
+    // MARK: - Init
+    init(logInUseCase: LogInUseCase,
+         actions: LogInViewModelActions? = nil) {
+        self.logInUseCase = logInUseCase
+        self.actions = actions
+    }
+}
+
+// MARK: - INPUT. View event methods
+extension DefaultLogInViewModel: LogInViewModelInput {
+    func didClickLogInWith(name: String, password: String) {
+     
+    }
+}
