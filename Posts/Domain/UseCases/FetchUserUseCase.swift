011bede1ec8dd674a45c854c34d4b7a8db538b9f
Mostafa-Mohsen <eng.mostafamohsen2016@gmail.com>
2025-03-18 22:30:55 +0200

    add fetch user use case request

---
+++ Posts/Domain/UseCases/FetchUserUseCase.swift
@@ -0,0 +1,15 @@
+//
+//  FetchUserUseCase.swift
+//  Posts
+//
+//  Created by M-M-M on 17/03/2025.
+//
+
+import Foundation
+import Combine
+
+
+
+struct FetchUsersUseCaseRequestValue {
+    let ids: [Int]
+}
