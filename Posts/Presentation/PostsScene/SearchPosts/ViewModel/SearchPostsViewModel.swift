d2eecaed4379328310341083fe4a10cf6689bfa6
Mostafa-Mohsen <eng.mostafamohsen2016@gmail.com>
2025-03-18 23:25:08 +0200

    add logic to search post, fetch user data and display the results

--- Posts/Presentation/PostsScene/SearchPosts/ViewModel/SearchPostsViewModel.swift
+++ Posts/Presentation/PostsScene/SearchPosts/ViewModel/SearchPostsViewModel.swift
@@ -72,17 +72,81 @@ final class DefaultSearchPostsViewModel: SearchPostsViewModelOutput {
         self.actions = actions
     }
 
+    // MARK: - Private
+    private func appendPage(postsPage: PostsPage, usersData: [Int: UserDataViewModel]) {
+        totalCount = postsPage.total
+        posts += postsPage.posts
+        skip = posts.count
+        let viewModelItems = postsPage.posts.map{ PostsListItemViewModel(post: $0, user: usersData[$0.userId ?? -1]) }
+        items.send(items.value + viewModelItems)
+        showListLoader.send(hasMorePages)
+        loading.send(.none)
+        if posts.isEmpty { isEmptyResults.send(true) }
+    }
+
+    private func resetPages() {
+        query = ""
+        skip = 0
+        totalCount = 0
+        showListLoader.send(false)
+        posts.removeAll()
+        items.send([])
+        loading.send(.none)
+        isEmptyResults.send(false)
+    }
+
+    private func loadPosts(with query: String, loading: SearchPostsViewModelLoading) {
+        self.loading.send(loading)
+        
+        searchPostsUseCase.execute(requestValue: .init(limit: limit, skip: skip, query: query))
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    print("All publishers completed successfully.")
+                case .failure(let error):
+                    self.loading.send(.none)
+                    
+                }
+            },
+            receiveValue: { postsPage in
+                guard let unwrappedPostsPage = postsPage else { return }
+                self.loadUsersFor(postPage: unwrappedPostsPage)
+            })
+            .store(in: &cancellable)
+    }
     
+    private func loadUsersFor(postPage: PostsPage) {
+        let ids = postPage.posts.compactMap{ $0.userId }
+        searchPostsUseCase.execute(requestValue: .init(ids: ids))
+            .sink() { usersData in
+                var usersDict = [Int: UserDataViewModel]()
+                usersData.compactMap { $0 }
+                    .forEach({
+                        usersDict[$0.id] = UserDataViewModel(firstName: $0.firstName, lastName: $0.lastName) })
+                self.appendPage(postsPage: postPage, usersData: usersDict)
+            }
+            .store(in: &cancellable)
+    }
+
+    
+
+    private func update(query: String) {
+        resetPages()
+        self.query = query
+        loadPosts(with: query, loading: .fullScreen)
+    }
 }
 
 // MARK: - INPUT. View event methods
 extension DefaultSearchPostsViewModel: SearchPostsViewModelInput {
     func didSearch(query: String) {
-        
+        guard !query.isEmpty, query != self.query else { return }
+        update(query: query)
     }
     
     func loadNextPage() {
-        
+        guard hasMorePages, loading.value == .none, !query.isEmpty else { return }
+        loadPosts(with: query, loading: .nextPage)
     }
     
     func didCancelSearch() {
