7a3d612d6d6f713a68563d437718dc5e4388dd75
Mostafa-Mohsen <eng.mostafamohsen2016@gmail.com>
2025-03-18 21:20:38 +0200

    add default posts use case

--- Posts/Domain/UseCases/FetchPostsUseCase.swift
+++ Posts/Domain/UseCases/FetchPostsUseCase.swift
@@ -12,6 +12,21 @@ protocol PostsListUseCase: FetchUserUseCase {
     func execute(requestValue: PostsUseCaseRequestValue) -> AnyPublisher<PostsPage?, NetworkError>
 }
 
+final class DefaultPostsUseCase {
+    private let postsRepository: PostsRepository
+    
+    init(postsRepository: PostsRepository) {
+        self.postsRepository = postsRepository
+    }
+}
+
+extension DefaultPostsUseCase: PostsListUseCase {
+    func execute(requestValue: PostsUseCaseRequestValue) -> AnyPublisher<PostsPage?, NetworkError> {
+        return postsRepository.getPosts(limit: requestValue.limit,
+                                        skip: requestValue.skip)
+    }
+}
+
 struct PostsUseCaseRequestValue {
     let limit: Int
     let skip: Int
